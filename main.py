4c1e76570ac739f74069195c7ef900fc81bb0314
HAtherlolz <kirill.syusko17@gmail.com>
2024-07-11 10:55:48 +0300

    added logs for bot starts and ends work

--- main.py
+++ main.py
@@ -1,16 +1,20 @@
-from telegram.ext import ApplicationBuilder, Updater, MessageHandler, filters
+from telegram.ext import ApplicationBuilder, MessageHandler, filters
 
 from cfg.config import settings
 from services.bot import Bot
+from utils.logs import log
 
 
 def main():
+    log.info("Bot starting ...")
     app = ApplicationBuilder().token(settings.TG_TOKEN).build()
 
     message_handler = MessageHandler(filters.TEXT & ~filters.COMMAND, Bot.handle_message)
     app.add_handler(message_handler)
 
+    log.info("Bot is running and listening")
     app.run_polling()
+    log.info("Bot is shut down")
 
 
 if __name__ == '__main__':
