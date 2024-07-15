d45a1b4d26ff9b5cd9e33ca9129522a2152a037e
HAtherlolz <kirill.syusko17@gmail.com>
2024-07-15 17:46:13 +0300

    new changes according the clients wishes

--- services/bot.py
+++ services/bot.py
@@ -15,32 +15,36 @@ class Bot:
             cls,
             update: Update,
             context: ContextTypes.DEFAULT_TYPE,
-    ):
+    ) -> None:
         chat_id = update.message.chat_id
         text = update.message.text
 
+        date_time = str(update.message.date)[:16]
+
         if text.startswith('#'):
             log.info(f"Message from chat {chat_id}: {text}")
-            parsed_msg = cls.message_parser(msg=text)
+            parsed_msg = cls.message_parser(msg=text, date_time=date_time)
             log.info(f"Parsed message: {parsed_msg}")
 
             Google.update_sht(parsed_msg)
 
     @staticmethod
-    def message_parser(msg: str) -> List:
+    def message_parser(msg: str, date_time: str) -> List:
         res = list()
         title = msg.split(" ")[0]
         title = title[1:-4]
         log.info(f"title: {title}")
 
         pattern = re.compile(
-            r"(?P<country>[A-Z]{2}(?: [A-Z]{2})?) - Total (?P<total_caps>\d+) cap - "
-            r"(?P<start_time>\d{2}:\d{2}) - (?P<end_time>\d{2}:\d{2})( gmt \+ \d+)?"
-            r"( - (?P<cpa>.*))?"
+            r"(?P<country>[A-Z]{2}(?: [A-Z]{2})?) - Total (?P<total_caps>\d+) cap"
+            r"( - (?P<start_time>\d{2}:\d{2}) - (?P<end_time>\d{2}:\d{2})( gmt \+ \d+)?)?"
+            r"( - (?P<cpa>.*))?",
+            re.IGNORECASE
         )
 
         for match in pattern.finditer(msg):
             res.append({
+                "date": date_time,
                 "title": title,
                 "country": match.group("country"),
                 "total_caps": match.group("total_caps"),
@@ -51,3 +55,4 @@ class Bot:
                 "note": match.group("cpa").split("\n")[0].split("-")[1] if match.group("cpa") else None  # Optional note
             })
         return res
+
--- services/google.py
+++ services/google.py
@@ -1,4 +1,4 @@
-from datetime import datetime
+from datetime import datetime, timedelta
 from typing import List
 
 import gspread
@@ -27,7 +27,10 @@ class Google:
             log.info(f"Worksheet '{sheet_name}' created.")
 
             # Adding titles to the newly created worksheet
-            titles = ["Affiliate Name", "GEO", "CAP", "Start Time", "End Time", "GMT", "CPA Cost", "Notes"]
+            titles = [
+                "Date and time", "Affiliate Name", "GEO", "CAP",
+                "Start Time", "End Time", "GMT", "CPA Cost", "Notes"
+            ]
             worksheet.append_row(titles)
         return worksheet
     
@@ -37,9 +40,20 @@ class Google:
         sht = gc.open_by_key(settings.GGL_SHEET_TOKEN)
 
         # Generate the worksheet name based on the current day and date
-        sheet_name = datetime.now().strftime("%A-%d-%m-%y")
-
+        sheet_name = cls._get_ggl_sheet_name()
         worksheet = cls.get_or_create_worksheet(sht, sheet_name)
 
         for row in data:
             worksheet.append_row(list(row.values()))
+
+    @classmethod
+    def _get_ggl_sheet_name(cls) -> str:
+        today = datetime.today()
+
+        start_of_week = today - timedelta(days=today.weekday())
+        end_of_week = start_of_week + timedelta(days=6)
+
+        start_date_str = start_of_week.strftime("%d.%m")
+        end_date_str = end_of_week.strftime("%d.%m")
+
+        return f"Mon-Sun {start_date_str[:2]}-{end_date_str}"
